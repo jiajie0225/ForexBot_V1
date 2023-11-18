9b2d2e6b400c4e69d9400434ab83b7c44ff94be7
jla510 <jla510@student.ubc.ca>
2023-11-17 21:40:30 -0800

    update main

--- infrastructure/instrument_collection.py
+++ infrastructure/instrument_collection.py
@@ -3,19 +3,18 @@ from models.instrument import Instrument
 
 class InstrumentCollection:
     FILENAME = "instruments.json"
-    API_KEYS = ['name', 'type', 'displayName', 'pipLocation', 'displayPrecision', 'tradeUnitsPrecision', 'marginRate']
+    API_KEYS = ['name', 'type', 'displayName', 'pipLocation',
+         'displayPrecision', 'tradeUnitsPrecision', 'marginRate']
 
-
-    def __init__(self) :
+    def __init__(self):
         self.instruments_dict = {}
-
-    
+        
     def LoadInstruments(self, path):
         self.instruments_dict = {}
         fileName = f"{path}/{self.FILENAME}"
         with open(fileName, "r") as f:
             data = json.loads(f.read())
-            for k,v in data.items():
+            for k, v in data.items():
                 self.instruments_dict[k] = Instrument.FromApiObject(v)
 
     def CreateFile(self, data, path):
@@ -23,20 +22,18 @@ class InstrumentCollection:
             print("Instrument file creation failed")
             return
         
-        instruments_dict= {}
+        instruments_dict = {}
         for i in data:
             key = i['name']
-            instruments_dict[key] = {k : i[k] for k in self.API_KEYS}
+            instruments_dict[key] = { k: i[k] for k in self.API_KEYS }
 
         fileName = f"{path}/{self.FILENAME}"
         with open(fileName, "w") as f:
             f.write(json.dumps(instruments_dict, indent=2))
 
+
     def PrintInstruments(self):
         [print(k,v) for k,v in self.instruments_dict.items()]
         print(len(self.instruments_dict.keys()), "instruments")
 
-
 instrumentCollection = InstrumentCollection()
-
-
