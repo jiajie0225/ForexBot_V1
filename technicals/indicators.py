bb4a9692ef28841112d5a1dfb83bb46b8cd293a0
jla510 <jla510@student.ubc.ca>
2023-12-04 21:26:24 -0800

    added Bollinger band and ATR

---
+++ technicals/indicators.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def BollingerBands(df: pd.DataFrame, n=20, s=2): # n for number of smoothing, s for number of standard deviation
+    typical_p = ( df.mid_c + df.mid_h + df.mid_l) / 3
+    stddev = typical_p.rolling(window=n).std()
+    df['BB_MA'] = typical_p.rolling(window=n).mean()
+    df['BB_UP'] = df['BB_MA'] + stddev * s
+    df['BB_LW'] = df['BB_MA'] - stddev * s
+    return df
+
+# Average True Range
+def ATR(df: pd.DataFrame, n=14):
+    prev_c = df.mid_c.shift(1)
+    # 3 true_ranges
+    tr1 = df.mid_h -df.mid_l
+    tr2 = abs(df.mid_h - prev_c)
+    tr3 = abs(prev_c - df.mid_l)
+    
+    tr = pd.DataFrame({'tr1':tr1, 'tr2':tr2, 'tr3':tr3}).max(axis=1)
+    df['ATR'] = tr.rolling(window=n).mean()
+    return df
+
+
+
+
+
+
